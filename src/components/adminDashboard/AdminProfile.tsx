b975778c42883987c1767067768ca729b88f4ee3
MIKAELA MUTHONI KAMUNYA <159615322+Icily57@users.noreply.github.com>
2025-04-02 10:16:27 +0300

    Update AdminProfile component layout and enhance form structure for better user experience

--- src/components/adminDashboard/AdminProfile.tsx
+++ src/components/adminDashboard/AdminProfile.tsx
@@ -56,95 +56,100 @@ const AdminProfile: React.FC = () => {
   return (
     <>
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-400 to-blue-950 p-6">
-      <div className="w-full max-w-3xl bg-blue-200 p-8 rounded-2xl shadow-lg transform hover:scale-[1.02] transition duration-300">
-          <h1 className="text-4xl font-bold text-gray-900 text-center mb-6 font-[Poppins]">Admin Profile 😎</h1>
-          <div className="flex items-center mb-6">
-            <div className="w-32 h-32 bg-black rounded-full overflow-hidden flex items-center justify-center">
-              <span className="text-4xl text-white font-semibold">{user?.full_name?.[0]}</span>
-            </div>
-            <div className="ml-6">
-              <h2 className="text-3xl font-semibold text-black">{user?.full_name}</h2>
-              <p className="text-xl text-black">{user?.email}</p>
-            </div>
+    <div className="w-full max-w-3xl bg-blue-200 p-8 rounded-2xl shadow-lg transform hover:scale-[1.02] transition duration-300">
+        <h1 className="text-4xl font-bold text-gray-900 text-center mb-6 font-[Poppins]">My Profile 🎉</h1>
+        <div className="flex items-center mb-6">
+          <div className="w-32 h-32 bg-black rounded-full overflow-hidden flex items-center justify-center">
+            <span className="text-4xl text-white font-semibold">{user?.full_name?.[0]}</span>
           </div>
-          <div className="space-y-6">
-            <div className="flex flex-col space-y-4">
-              <label htmlFor="full_name" className="text-sm font-semibold text-black">Full Name</label>
-              <input
-                type="text"
-                id="full_name"
-                name="full_name"
-                value={formData.full_name}
-                onChange={handleInputChange}
-                className={`px-4 py-3 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-300 text-blue-950 ${isEditing ? 'bg-white' : 'bg-blue-50'}`}
-                readOnly={!isEditing}
-              />
-            </div>
-            <div className="flex flex-col space-y-4">
-              <label htmlFor="email" className="text-sm font-semibold text-gray-800">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                className={`px-4 py-3 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-300 text-blue-950 ${isEditing ? 'bg-white' : 'bg-blue-50'}`}
-                readOnly={!isEditing}
-              />
-            </div>
-            <div className="flex flex-col space-y-4">
-              <label htmlFor="contact_phone" className="text-sm font-semibold text-gray-800">Contact Phone</label>
-              <input
-                type="text"
-                id="contact_phone"
-                name="contact_phone"
-                value={formData.contact_phone}
-                onChange={handleInputChange}
-                className={`px-4 py-3 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-300 text-blue-950 ${isEditing ? 'bg-white' : 'bg-blue-50'}`}
-                readOnly={!isEditing}
-              />
-            </div>
-            <div className="flex flex-col space-y-4">
-              <label htmlFor="address" className="text-sm font-semibold text-gray-800">Address</label>
-              <input
-                type="text"
-                id="address"
-                name="address"
-                value={formData.address}
-                onChange={handleInputChange}
-                className={`px-4 py-3 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-300 text-blue-950 ${isEditing ? 'bg-white ' : 'bg-blue-50'}`}
-                readOnly={!isEditing}
-              />
-            </div>
-            <div className="flex justify-end space-x-4 mt-6">
-              {isEditing ? (
-                <>
-                  <button
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-lg transition ease-in-out duration-300"
-                    onClick={handleSave}
-                  >
-                    Save
-                  </button>
-                  <button
-                    className="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-6 rounded-lg transition ease-in-out duration-300"
-                    onClick={handleCancel}
-                  >
-                    Cancel
-                  </button>
-                </>
-              ) : (
+          <div className="ml-6">
+            <h2 className="text-3xl font-semibold text-black">{user?.full_name}</h2>
+            <p className="text-xl text-black">{user?.email}</p>
+          </div>
+        </div>
+        <div className="space-y-6">
+          <div className="flex flex-row justify-evenly items-center mb-4">
+          <div className="flex flex-col mr-2 space-y-4">
+            <label htmlFor="full_name" className="text-sm font-semibold text-black">Full Name</label>
+            <input
+              type="text"
+              id="full_name"
+              name="full_name"
+              value={formData.full_name}
+              onChange={handleInputChange}
+              className={`px-4 py-3 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-300 text-blue-950 ${isEditing ? 'bg-white' : 'bg-blue-50'}`}
+              readOnly={!isEditing}
+            />
+          </div>
+          <div className="flex flex-col space-y-4">
+            <label htmlFor="email" className="text-sm font-semibold text-gray-800">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className={`px-4 py-3 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-300 text-blue-950 ${isEditing ? 'bg-white' : 'bg-blue-50'}`}
+              readOnly={!isEditing}
+            />
+          </div>
+          </div>
+          <div className="flex flex-row justify-evenly items-center mb-4">
+          <div className="flex flex-col space-y-4">
+            <label htmlFor="contact_phone" className="text-sm font-semibold text-gray-800">Contact Phone</label>
+            <input
+              type="text"
+              id="contact_phone"
+              name="contact_phone"
+              value={formData.contact_phone}
+              onChange={handleInputChange}
+              className={`px-4 py-3 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-300 text-blue-950 ${isEditing ? 'bg-white' : 'bg-blue-50'}`}
+              readOnly={!isEditing}
+            />
+          </div>
+          <div className="flex flex-col space-y-4">
+            <label htmlFor="address" className="text-sm font-semibold text-gray-800">Address</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              value={formData.address}
+              onChange={handleInputChange}
+              className={`px-4 py-3 border border-blue-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition ease-in-out duration-300 text-blue-950 ${isEditing ? 'bg-white ' : 'bg-blue-50'}`}
+              readOnly={!isEditing}
+            />
+          </div>
+          </div>
+          <div className="flex justify-end space-x-4 mt-6">
+            {isEditing ? (
+              <>
                 <button
                   className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-lg transition ease-in-out duration-300"
-                  onClick={handleEditClick}
+                  onClick={handleSave}
                 >
-                  Edit
+                  Save
                 </button>
-              )}
-            </div>
+                <button
+                  className="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-6 rounded-lg transition ease-in-out duration-300"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </button>
+              </>
+            ) : (
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-lg transition ease-in-out duration-300"
+                onClick={handleEditClick}
+              >
+                Edit
+              </button>
+            )}
           </div>
         </div>
       </div>
-    </>
+    </div>
+    {/* <Footer /> */}
+  </>
   );
 };
 
